39c8c7f38c946ab5aacab2c8f1b789c4db9ff6f9
Roman Gorelik <romang31@gmail.com>
2018-11-21 13:27:47 -0500

    Added a touch of styling to make it more readable

--- client/src/Components/Article.jsx
+++ client/src/Components/Article.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 
+const styles = {
+  article: {
+    paddingBottom: 10,
+    borderBottomStyle: 'solid',
+    borderBottomColor: '#aaa',
+    borderBottomWidth: 1,
+    marginBottom: 10
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  date: {
+    fontSize: '0.05em',
+    color: '#888'
+  },
+  author: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  body: {
+    paddingLeft: 20
+  }
+}
+
 const Article = (props) => {
   const {article, author} = props
   return (
-    <div>
-      <div>{article.title}</div>
-      <div>{article.date}</div>
-      <div>
+    <div style={styles.article}>
+      <div style={styles.title}>{article.title}</div>
+      <div style={styles.date}>{article.date}</div>
+      <div style={styles.author}>
         <a href={author.website} target='_blank'>
           {author.firstName} {author.lastName}
         </a>
       </div>
-      <div>{article.body}</div>
+      <div style={styles.body}>{article.body}</div>
     </div>
   )
 }
